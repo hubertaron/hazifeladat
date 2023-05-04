c4c0d5688288865bfbe187e54a701440e22390af
hubertaron <130552838+hubertaron@users.noreply.github.com>
2023-05-04 08:48:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string eleresiUt = @"D:\husi\"; 
+        string kiterjesztes = ".txt"; // a keresett fájlkiterjesztés
+
+        DateTime creationDate = new DateTime(2023, 5, 3); // a keresett létrehozási dátum (év, hónap, nap)
+
+        string[] files = Directory.GetFiles(eleresiUt, "*" + kiterjesztes, SearchOption.AllDirectories);
+
+        using (StreamWriter writer = new StreamWriter("files.txt"))
+        {
+            foreach (string file in files)
+            {
+                DateTime fileCreationDate = File.GetCreationTime(file);
+                if (fileCreationDate >= creationDate)
+                {
+                    writer.WriteLine("{0}; {1}", Path.GetFileName(file), fileCreationDate);
+                }
+            }
+        }
+    }
+}
